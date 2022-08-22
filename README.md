ca6fc9f983a46b32e351d1c41d4a4575bb4f0fab
MertKelleci <mertkelleci99@gmail.com>
2022-08-22 19:49:56 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# SysTop App
+
+## What Is This App
+
+This app can display your computer's info, track certain values like your CPU use percentage and warns you when your overreach a certain threshold.
+
+## Technologies Used
+
+- HTML, CSS and vanilla JavaScript.
+- Electron.js to package as a desktop application.
+
+## Images
+
+<figure>
+    <img src="Images/1.png" width="300px"/>
+</figure>
+<figure>
+ <img src="Images/2.png" width="300px"/>
+</figure>
+<figure>
+<img src="Images/3.png" width="300px"/>
+</figure>
---
+++ main.js
@@ -0,0 +1,127 @@
+const { app, BrowserWindow, Menu, ipcMain, Tray } = require("electron");
+const log = require("electron-log");
+const Store = require("./Store");
+const path = require("path");
+// Set env
+process.env.NODE_ENV = "production";
+
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
+
+let mainWindow;
+let tray;
+
+const store = new Store({
+  configName: "user-settings",
+  defaults: {
+    settings: {
+      cpuOverload: 80,
+      alertFrequency: 5,
+    },
+  },
+});
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    title: "SysTop",
+    width: isDev ? 800 : 500,
+    height: 600,
+    icon: "./assets/icons/icon.png",
+    resizable: isDev ? true : false,
+    show: isDev,
+    // opacity: 0.9,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  });
+
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.loadFile("./app/index.html");
+}
+
+app.on("ready", () => {
+  createMainWindow();
+
+  mainWindow.webContents.on("dom-ready", () => {
+    mainWindow.webContents.send("settings:get", store.get("settings"));
+  });
+
+  mainWindow.on("close", (e) => {
+    if (!app.isQuiting) {
+      e.preventDefault();
+      mainWindow.hide();
+    }
+
+    return true;
+  });
+  const icon = path.join(__dirname, "assets", "icons", "tray_icon.png");
+  tray = new Tray(icon);
+  tray.on("click", () => {
+    if (mainWindow.isVisible() === true) {
+      mainWindow.hide();
+    } else {
+      mainWindow.show();
+    }
+  });
+
+  tray.on("right-click", () => {
+    const contextMenu = Menu.buildFromTemplate([
+      {
+        label: "Quit",
+        click: () => {
+          app.isQuiting = true;
+          app.quit();
+        },
+      },
+    ]);
+
+    tray.popUpContextMenu(contextMenu);
+  });
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+});
+
+const menu = [
+  ...(isMac ? [{ role: "appMenu" }] : []),
+  {
+    role: "fileMenu",
+  },
+  ...(isDev
+    ? [
+        {
+          label: "Developer",
+          submenu: [
+            { role: "reload" },
+            { role: "forcereload" },
+            { type: "separator" },
+            { role: "toggledevtools" },
+          ],
+        },
+      ]
+    : []),
+];
+
+ipcMain.on("settings:set", (e, value) => {
+  store.set("settings", value);
+  mainWindow.webContents.send("settings:get", store.get("settings"));
+});
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
+
+app.allowRendererProcessReuse = true;
